bd99e03fc7bc922ad1181eb2b6fddb750488be54
Michael <[johnmichael.camara.git@gmail.com]>
2023-10-30 18:10:38 +0800

    added table, buttons, new folder and minor changes

--- homepage.php
+++ homepage.php
@@ -21,37 +21,142 @@
                         <a class="nav-link" href="#">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
+                        <a class="nav-link" href="#">List</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">List</a>
+                        <a class="nav-link" href="#">Archive</a>
                     </li>
                 </ul>
 
             </div>
         </div>
     </nav>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+    <div class="table-container table-responsive">
+        <table class="table table-borderless table-hover  table-responsive">
+            <thead class=" table-dark">
+                <tr>
+                    <th scope="col">My Tasks</th>
+                    <th scope="col">Start Date</th>
+                    <th scope="col">Due Date</th>
+                    <th scope="col">Complete%</th>
+                    <th scope="col">Progress</th>
+                    <th scope="col">Notes</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Finish Homework</td>
+                    <td>Date</td>
+                    <td>Date</td>
+                    <td>50%</td>
+                    <td>Incomplete</td>
+                    <td>I will finish this</td>
+                </tr>
+                <tr>
+                    <td>Finish Homework</td>
+                    <td>Date</td>
+                    <td>Date</td>
+                    <td>50%</td>
+                    <td>Incomplete</td>
+                    <td>I will finish this</td>
+                </tr>
+                <tr>
+                    <td>Finish Homework</td>
+                    <td>Date</td>
+                    <td>Date</td>
+                    <td>50%</td>
+                    <td>Incomplete</td>
+                    <td>I will finish this</td>
+                </tr>
+                <tr>
+                    <td>Finish Homework</td>
+                    <td>Date</td>
+                    <td>Date</td>
+                    <td>50%</td>
+                    <td>Incomplete</td>
+                    <td>I will finish this</td>
+                </tr>
+                <tr>
+                    <td>Finish Homework</td>
+                    <td>Date</td>
+                    <td>Date</td>
+                    <td>50%</td>
+                    <td>Incomplete</td>
+                    <td>I will finish this</td>
+                </tr>
+                <tr>
+                    <td>Finish Homework</td>
+                    <td>Date</td>
+                    <td>Date</td>
+                    <td>50%</td>
+                    <td>Incomplete</td>
+                    <td>I will finish this</td>
+                </tr>
+                <tr>
+                    <td>Finish Homework</td>
+                    <td>Date</td>
+                    <td>Date</td>
+                    <td>50%</td>
+                    <td>Incomplete</td>
+                    <td>I will finish this</td>
+                </tr>
+                <tr>
+                    <td>Finish Homework</td>
+                    <td>Date</td>
+                    <td>Date</td>
+                    <td>50%</td>
+                    <td>Incomplete</td>
+                    <td>I will finish this</td>
+                </tr>
+                <tr>
+                    <td>Finish Homework</td>
+                    <td>Date</td>
+                    <td>Date</td>
+                    <td>50%</td>
+                    <td>Incomplete</td>
+                    <td>I will finish this</td>
+                </tr>
+                <tr>
+                    <td>Finish Homework</td>
+                    <td>Date</td>
+                    <td>Date</td>
+                    <td>50%</td>
+                    <td>Incomplete</td>
+                    <td>I will finish this</td>
+                </tr>
+                <tr>
+                    <td>Finish Homework</td>
+                    <td>Date</td>
+                    <td>Date</td>
+                    <td>50%</td>
+                    <td>Incomplete</td>
+                    <td>I will finish this</td>
+                </tr>
+            </tbody>
+            <tfoot>
+                <th>
+                    <button type="button" class="btn custom-button-add">
+                        Add
+                    </button>
+                </th>
+                <td>
+                    <button type="button" class="btn custom-button-edit">Edit</button>
+                </td>
+                <td>
+                    <button type="button" class="btn custom-button-del">Delete</button>
+                </td>
+                <td>
+                    <button type="button" class="btn custom-button-arch">Archive</button>
+                </td>
+                <td>
 
+                </td>
+                <td>
+
+                </td>
+            </tfoot>
+        </table>
+    </div>
     <script src="./bootstrap-5.3.1-dist/js/bootstrap.js">
 
     </script>
