5a6f669a9d4c868d09abbd20b99bfad837fb7cd3
John Michael Camara <johnmichael.camara.git@gmail.com>
2023-11-23 10:57:39 +0800

    Added connection for localhost

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "todolist_db";
+$databasename = "todolist_db";
 
-$conn = new mysqli($servername,$username, $password, $dbname);
+$conn = new mysqli($servername,$username, $password, $databasename);
 ?>
